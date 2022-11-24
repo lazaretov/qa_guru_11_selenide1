225d68c5726695ad0c25c262dd93c05f02b621ea
Timofei <lazaretov03@gmail.com>
2022-11-24 13:01:34 +0300

    upd build.gradle Add test

--- src/test/java/SelenideSearchTest.java
+++ src/test/java/SelenideSearchTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 
@@ -8,6 +10,11 @@ public class SelenideSearchTest {
     void shouldFindJunit5Example(){
         open("https://github.com/selenide/selenide");
         $("#wiki-tab").click();
-
+        $(".markdown-body ul").shouldHave(text("Soft assertions"));
+        $$(".markdown-body").findBy(visible)
+                .$$("ul li a").findBy(text("Soft assertions")).click();
+        //$(byText("Soft assertions")).click();
+        $("#user-content-3-using-junit5-extend-test-class").closest("h4").sibling(0)
+                .$("pre").shouldBe(visible);
     }
 }
