6f19ae09ba0163e6e580b8abb263e6d6b50689e3
Timofei <lazaretov03@gmail.com>
2022-11-25 11:33:55 +0300

    fix test

--- src/test/java/SelenideSearchTest.java
+++ src/test/java/SelenideSearchTest.java
@@ -1,20 +1,17 @@
 import org.junit.jupiter.api.Test;
-
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 
 public class SelenideSearchTest {
     @Test
-    void shouldFindJunit5Example(){
+    void shouldFindJunit5Example() {
         open("https://github.com/selenide/selenide");
         $("#wiki-tab").click();
         $(".markdown-body ul").shouldHave(text("Soft assertions"));
-        $$(".markdown-body").findBy(visible)
-                .$$("ul li a").findBy(text("Soft assertions")).click();
-        //$(byText("Soft assertions")).click();
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $("#wiki-pages-box").$$("ul").findBy(text("SoftAssertions")).click();
         $("#user-content-3-using-junit5-extend-test-class").closest("h4").sibling(0)
-                .$("pre").shouldBe(visible);
+                .$("pre").shouldHave(text("{SoftAssertsExtension.class}"));
     }
 }
